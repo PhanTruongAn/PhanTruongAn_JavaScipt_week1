49adea3726625815381ec1eb6c0daa2296015342
PhanTruongAn <phanan1000@gmail.com>
2023-09-12 13:11:04 +0700

    first

---
+++ Buoi1.js
@@ -0,0 +1,23 @@
+// 1. Store Mark's and John's mass and height in variables
+var markMass = 70;
+var markHeight = 1.75;
+
+var johnMass = 80;
+var johnHeight = 1.80;
+
+// 2. Calculate both their BMIs using the formula
+var markBMI = markMass / (markHeight * markHeight);
+var johnBMI = johnMass / (johnHeight * johnHeight);
+
+// 3. Create a Boolean variable 'markHigherBMI'
+var markHigherBMI = markBMI > johnBMI;
+
+// Output the results
+console.log("Mark's BMI: " + markBMI);
+console.log("John's BMI: " + johnBMI);
+
+if (markHigherBMI) {
+    console.log("Mark has a higher BMI than John.");
+} else {
+    console.log("John has a higher or equal BMI compared to Mark.");
+}
